d255d2007551157eb75ab95a4d972b21bc3238d1
Ksenija Gogic <ksenija.gogic@instacart.com>
2022-12-08 00:03:35 -0500

    Day 4

--- day-4/app.js
+++ day-4/app.js
@@ -9,14 +9,8 @@ function toTuples(pairLine) {
   return pairLine.split(',').map(toTuple)
 }
 
+
 function compareTuplesForCompleteOverlap(tuples) {
-  // Sort tuples so the one with the smallest number is first, then we can compare
-  const needsResorting = tuples[0][0] > tuples[1][0]
-  const tuplesResorted = needsResorting ? [tuples[1], tuples[0]] : tuples
-  const indexSingleRangeTuple = singleRangeTuple(tuplesResorted)
-
-  // console.log(tuples, tuplesResorted)
-
   const a = tuples[0][0]
   const b = tuples[0][1]
   const c = tuples[1][0]
@@ -25,14 +19,14 @@ function compareTuplesForCompleteOverlap(tuples) {
   return a <= c && b >= d || a >= c && b <= d
 }
 
-function singleRangeTuple(tuples) {
-  let indexDupe = -1
-  tuples.forEach((tuple, i) => {
-    if(tuple[0] === tuple[1]) {
-      indexDupe = i
-    }
-  })
-  return indexDupe
+function compareTuplesForPartialOverlap(tuples) {
+  const needsResorting = tuples[0][0] > tuples[1][0]
+  const tuplesResorted = needsResorting ? [tuples[1], tuples[0]] : tuples
+
+  const b = tuplesResorted[0][1]
+  const c = tuplesResorted[1][0]
+
+  return b >= c
 }
 
 async function solutionPt1() {
@@ -52,5 +46,24 @@ async function solutionPt1() {
   console.log(completeOverlapOccurrences)
 }
 
+async function solutionPt2() {
+  const lineReader = await readFileLineByLine('./input.txt')
+  let partialOverlapOccurrences = 0
+
+  for await (const line of lineReader) {
+    const tuples = toTuples(line)
+    const hasOverlap = compareTuplesForPartialOverlap(tuples)
+
+    console.log(tuples, hasOverlap)
+
+    if (hasOverlap) {
+      partialOverlapOccurrences = partialOverlapOccurrences + 1
+    }
+  }
+
+  console.log(partialOverlapOccurrences)
+}
+
 solutionPt1()
+solutionPt2()
 
